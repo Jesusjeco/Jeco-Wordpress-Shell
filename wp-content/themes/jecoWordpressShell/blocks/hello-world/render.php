60afa6119a2ce2b8b4c8c5cce9990b34c28993a9
Jesusjeco <jesusenrique.carrero@gmail.com>
2024-08-07 15:10:13 -0400

    Adding a test block for starting

---
+++ wp-content/themes/jecoWordpressShell/blocks/hello-world/render.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="<?= BLOCKS_STYLES_PATH . "/hello-world/style.css" ?>">
+<?php
+$data = get_field('data');
+?>
+<div class="hello-world">
+  <h1>Render block</h1>
+  <p>This is the render block</p>
+</div>
